0e5708e016c913c37e90cd69099a58ef6a4a9291
MalakHamed5 <mh5627709@gmail.com>
2026-03-27 21:58:23 +0200

    Flutter project for an e-commerce application.

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:ecommerse/malak_market.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MalakMarket());
+}
---
+++ lib/malak_market.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class MalakMarket extends StatelessWidget {
+  const MalakMarket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
